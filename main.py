dada3b9e613b3e9c322378597a9109ae01e29c9d
Dr. Qiaoli Huang <huang@nilpferd.cbs.mpg.de>
2023-05-03 15:58:53 +0200

    scripts01

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello world')
+import os.path as op
+import os
+# import sys
+import numpy as np
+import pandas as pd
+import mne
+
+data_path='/data/pt_02783/memory_grid/rawdir/mg99a'
+
+raw_fname_in=op.join(data_path,'mg99a01'+'.fif')
+
+raw = mne.io.read_raw_fif(
+    raw_fname_in,
+    allow_maxshield=True, 
+    preload=False, 
+    verborse= True)
+
